26781c20b891672d52e129ccbbcf395b5e15f3b5
Carolin Schwarz <schwarzcarolin262@gmail.com>
2017-12-23 18:11:08 +0100

    no plots overlapping anymore

--- main.py
+++ main.py
@@ -35,58 +35,64 @@ class Feedback_client:
             "f10", "f11", "f12", "f13", "f14", "f15","f16"]
         """
 
+        self.plot_gender_pie(self.se_dataframe, 'gender', 'Geschlecht_der_Studis')
+
+        self.plot_gender_pie(self.se_dataframe, 'tutor', 'Tutor_der_Studis')
+
+        self.plot_gender_pie(self.se_dataframe, 'fach', 'Studiengang_der_Studis')
+
         self.plot_barchart(self.se_dataframe, self.sort_column(self.se_dataframe['semester'])[0],
                            self.sort_column(self.se_dataframe['semester'])[1],
-                           "Semester der Studierenden", "Semester", "Anzahl an Studis")
+                           "Semester_der_Studierenden", "Semester", "Anzahl an Studis")
 
         self.plot_barchart(self.se_dataframe, self.sort_column(self.se_dataframe['f2'])[0],
                            self.sort_column(self.se_dataframe['f2'])[1],
-                           "Tutor kann den Lehrinhalt verstaendlich darlegen",
+                           "Tutor_kann_den_Lehrinhalt_verstaendlich_darlegen",
                            "Verstaendnis im Tutorium", "Anzahl an Studis")
 
         self.plot_barchart(self.se_dataframe, self.sort_column(self.se_dataframe['f3'])[0],
                            self.sort_column(self.se_dataframe['f3'])[1],
-                           "Tutor ist immer gut auf das Tutorium vorbereitet",
+                           "Tutor_ist_immer_gut_auf_das_Tutorium_vorbereitet",
                            "Vorbereitung", "Anzahl an Studis")
 
         self.plot_barchart(self.se_dataframe, self.sort_column(self.se_dataframe['f4'])[0],
                            self.sort_column(self.se_dataframe['f4'])[1],
-                           "Tutor foerdert aktive Teilnahme der Studenten",
+                           "Tutor_foerdert_aktive_Teilnahme_der_Studenten",
                            "Aktive Teilnahme", "Anzahl an Studis")
 
         self.plot_barchart(self.se_dataframe, self.sort_column(self.se_dataframe['f5'])[0],
                            self.sort_column(self.se_dataframe['f5'])[1],
-                           "Ich war immer gut auf das Tutorium vorbereitet.",
+                           "Ich_war_immer_gut_auf_das_Tutorium_vorbereitet",
                            "Vorbereitung der Studis", "Anzahl an Studis")
 
         self.plot_barchart(self.se_dataframe, self.sort_column(self.se_dataframe['f6'])[0],
                            self.sort_column(self.se_dataframe['f6'])[1],
-                           "Die Uebungen sind gut strukturiert",
+                           "Die_Uebungen_sind_gut_strukturiert",
                            "Struktur des Tutorium", "Anzahl an Studis")
 
         self.plot_barchart(self.se_dataframe, self.sort_column(self.se_dataframe['f7'])[0],
                            self.sort_column(self.se_dataframe['f7'])[1],
-                           "Die Uebungsbeispiele sind gut gewaehlt",
+                           "Die_Uebungsbeispiele_sind_gut_gewaehlt",
                            "Beispiele sind gut", "Anzahl an Studis")
 
         self.plot_barchart(self.se_dataframe, self.sort_column(self.se_dataframe['f8'])[0],
                            self.sort_column(self.se_dataframe['f8'])[1],
-                           "Der Schwierigkeitsgrad war hoch",
+                           "Der_Schwierigkeitsgrad_war_hoch",
                            "Zu schwierig", "Anzahl an Studis")
 
         self.plot_barchart(self.se_dataframe, self.sort_column(self.se_dataframe['f9'])[0],
                            self.sort_column(self.se_dataframe['f9'])[1],
-                           "Ich habe mich schon fruehzeitig mit dem Programm zu Hause beschaeftigt",
+                           "Ich_habe_mich_schon_fruehzeitig_mit_dem_Programm_zu_Hause_beschaeftigt",
                            "Fruehes, eigenstaendiges Arbeiten der Studis", "Anzahl an Studis")
 
         self.plot_barchart(self.se_dataframe, self.sort_column(self.se_dataframe['f10'])[0],
                            self.sort_column(self.se_dataframe['f10'])[1],
-                           "Ich habe die Uebungsaufgaben alle gemacht",
+                           "Ich_habe_die_Uebungsaufgaben_alle_gemacht",
                            "Fleissige Studis", "Anzahl an Studis")
 
         self.plot_barchart(self.se_dataframe, self.sort_column(self.se_dataframe['f11'])[0],
                            self.sort_column(self.se_dataframe['f11'])[1],
-                           "Ich haette gerne mehr Uebungsaufgaben gehabt",
+                           "Ich_haette_gerne_mehr_Uebungsaufgaben_gehabt",
                            "Unterforderte Studis", "Anzahl an Studis")
 
         self.plot_barchart(self.se_dataframe, self.sort_column(self.se_dataframe['f12'])[0],
@@ -96,29 +102,24 @@ class Feedback_client:
 
         self.plot_barchart(self.se_dataframe, self.sort_column(self.se_dataframe['f13'])[0],
                            self.sort_column(self.se_dataframe['f13'])[1],
-                           "Der Lehrinhalt war ausreichend um die HA zu bearbeiten",
+                           "Der_Lehrinhalt_war_ausreichend_um_die_HA_zu_bearbeiten",
                            "Hausaufgaben vorbereitung war ausreichend", "Anzahl an Studis")
 
         self.plot_barchart(self.se_dataframe, self.sort_column(self.se_dataframe['f14'])[0],
                            self.sort_column(self.se_dataframe['f14'])[1],
-                           "Ich habe viel Zeit zur Bearbeitung der Hausaufgabe benoetigt",
+                           "Ich_habe_viel_Zeit_zur_Bearbeitung_der_Hausaufgabe_benoetigt",
                            "langsame Studenten", "Anzahl an Studis")
 
         self.plot_barchart(self.se_dataframe, self.sort_column(self.se_dataframe['f15'])[0],
                            self.sort_column(self.se_dataframe['f15'])[1],
-                           "Die Hausaufgabe sollte vom Umfang her reduziert werden",
+                           "Die_Hausaufgabe_sollte_vom_Umfang_her_reduziert_werden",
                            "Zuviele Hausaufgaben", "Anzahl an Studis")
 
         self.plot_barchart(self.se_dataframe, self.sort_column(self.se_dataframe['f16'])[0],
                            self.sort_column(self.se_dataframe['f16'])[1],
-                           "Benoetigte Stundenzahl vom gesamten CAD-Kurs pro Woche",
+                           "Benoetigte_Stundenzahl_vom_gesamten_CAD_Kurs_pro_Woche",
                            "Stunden pro Woche", "Anzahl an Studis")
 
-        self.plot_gender_pie(self.se_dataframe, 'gender', 'Geschlecht_der_Studis')
-
-        self.plot_gender_pie(self.se_dataframe, 'tutor', 'Tutor_der_Studis')
-
-        self.plot_gender_pie(self.se_dataframe, 'fach', 'Studiengang_der_Studis')
 
 
 
@@ -137,32 +138,42 @@ class Feedback_client:
 
     def plot_barchart(self,dataframe,x,y,title,xlable,ylable):
         plt.bar(x, y, color='blue')
-        plt.title(title)
+        #plt.title(title)
         plt.xlabel(xlable)
         plt.ylabel(ylable)
         plt.savefig('./PDFcreater/Plots/{}.png'.format(title))
-        #plt.show()
+        #loescht den Plot fuer den Naechsten Plot
+        plt.clf()
+        plt.cla()
+        plt.close()
 
     def plot_scatter(self,dataframe,x,y,title,xlable,ylable):
         plt.scatter(x, y)
-        plt.title(title)
+        #plt.title(title)
         plt.xlabel(xlable)
         plt.ylabel(ylable)
         plt.savefig('./PDFcreater/Plots/{}.png'.format(title))
         plt.show()
 
-    def plot_gender_pie(self,se_dataframe,topic,se_title):
-        title(se_title, bbox={'facecolor': 'white'})
+    def plot_scatter_topic(self,se_dataframe,topic,se_title):
         gender = self.sort_column(se_dataframe[topic])
-        figure(1, figsize=(6, 6))
-        #ax = axes([0, 0, 0, 0])
+        labels = [gender[0][i] for i, elem in enumerate(gender[0])]
+        fracs = [gender[1][i] for i, elem in enumerate(gender[1])]
+        explode = [0.05 for i, elem in enumerate(gender[1])]
+        plt.scatter([i for i, elem in enumerate(gender[1])], fracs)
+        #plt.pie(fracs, explode=explode, labels=labels, autopct='%.0f%%', shadow=True)
+        plt.savefig('./PDFcreater/Plots/{}.png'.format(se_title))
+
+    def plot_gender_pie(self,se_dataframe,topic,se_title):
+        gender = self.sort_column(se_dataframe[topic])
         labels = [gender[0][i] for i,elem in enumerate(gender[0])]
         fracs = [gender[1][i] for i,elem in enumerate(gender[1])]
         explode = [0.05 for i,elem in enumerate(gender[1])]
-        pie(fracs, explode=explode, labels=labels,
-             shadow=True, startangle=90)
+        plt.pie(fracs, explode=explode, labels=labels, autopct='%.0f%%', shadow=True)
         plt.savefig('./PDFcreater/Plots/{}.png'.format(se_title))
-
+        plt.clf()
+        plt.cla()
+        plt.close()
 
 
 """
